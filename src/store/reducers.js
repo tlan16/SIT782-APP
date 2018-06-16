af90196947393e00b5e3774081eba383e17938e6
kathy y <meimeiy1121@gmail.com>
2018-06-16 18:19:34 +1000

    root reducer stores all fetched propertie with action type FETCH_ALL_PROPERTIES

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,9 +1,24 @@
+import { FETCH_ALL_PROPERTIES }from '../actionTypes'
+
 const initialState = {
   results: [],
   saved: []
 }
 
-const root = (state = initialState, action) => {
-  return state
+const createResultList = allProperties => allProperties.results;
+
+const createSavedList = allProperties => allProperties.saved;
+
+const root = (state = initialState, action = {}) => {
+  switch(action.type){
+    case  FETCH_ALL_PROPERTIES:
+       return {
+         results: createResultList(action.allProperties),
+         saved: createSavedList(action.allProperties)
+       }
+    default:
+       return state;
+  }
+  
 }
 export default root
