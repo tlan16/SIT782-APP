43e077c55c5c7a4fe48eabdaded7f4fe77586622
kathy y <meimeiy1121@gmail.com>
2018-06-16 15:30:35 +1000

    original state for property and category

---
+++ src/store/category/reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  results: [],
+  saved: []
+}
+
+const category = (state = initialState, action) => {
+    return state
+}
+export default category
---
+++ src/store/property/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  id: undefined,
+  price: undefined,
+  agency: undefined,
+  mainImage: undefined,
+  saved: false,
+}
+
+const property = (state = initialState, action) => {
+   return state
+}
+
+export default property;
