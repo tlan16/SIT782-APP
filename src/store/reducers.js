08b0229c03a27f2e92d3ed36bf40c3c09094ad52
kathy y <meimeiy1121@gmail.com>
2018-06-16 19:54:29 +1000

    delete root reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,24 +1,5 @@
-import { FETCH_ALL_PROPERTIES }from '../actionTypes'
 
-const initialState = {
-  results: [],
-  saved: []
-}
 
-const createResultList = allProperties => allProperties.results;
 
-const createSavedList = allProperties => allProperties.saved;
-
-const root = (state = initialState, action = {}) => {
-  switch(action.type){
-    case  FETCH_ALL_PROPERTIES:
-       return {
-         results: createResultList(action.allProperties),
-         saved: createSavedList(action.allProperties)
-       }
-    default:
-       return state;
-  }
   
-}
-export default root
+
