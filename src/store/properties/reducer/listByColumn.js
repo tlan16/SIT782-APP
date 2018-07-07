f11ebc5799821c0a78881d15d275e9d41d3ec3e8
kathy y <meimeiy1121@gmail.com>
2018-07-07 11:10:32 +1000

    fix the issue of adding existing property to saved list

--- src/store/properties/reducer/listByColumn.js
+++ src/store/properties/reducer/listByColumn.js
@@ -1,40 +1,50 @@
-import { PROPERTIES_FETCHED, ADD_TO_SAVED, REMOVE_FROM_SAVED } from "../actionTypes";
-import Immutable from 'seamless-immutable';
+import {
+  PROPERTIES_FETCHED,
+  ADD_TO_SAVED,
+  REMOVE_FROM_SAVED
+} from "../actionTypes";
+import Immutable from "seamless-immutable";
 
 const initialState = Immutable({
   results: [],
-  saved:[]
-})
-const listByColumn = ( state = initialState, action = {} ) => {
-   switch( action.type ){
-     case PROPERTIES_FETCHED:
-          return action.lists
+  saved: []
+});
 
-     case ADD_TO_SAVED:
-          const idToAdd = action.id;
-          return {
-            ...state,
-            saved: [...state.saved, idToAdd]
-          }
+const listByColumn = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case PROPERTIES_FETCHED:
+      return action.lists;
 
-      case REMOVE_FROM_SAVED:
-          const idToDelete = action.id;
-          const indexToDelete = state.saved.indexOf(idToDelete);
-          const savedList = state.saved;
-          if(indexToDelete == -1) return state
-          return {
-            ...state,
-            saved: [...savedList.slice(0, indexToDelete), ...savedList.slice(indexToDelete+1)]
-          }
-     default:
-          return state;
-   }
-}
-export default listByColumn
+    case ADD_TO_SAVED:
+      const idToAdd = action.id;
+      if (state.saved.indexOf(idToAdd) == -1)
+        return {
+          ...state,
+          saved: [...state.saved, idToAdd]
+        };
+      else return state;
 
-export const getSavedProperties = ( state ) => {
-    return state.saved;
-}
-export const getResultsProperties = ( state ) => {
+    case REMOVE_FROM_SAVED:
+      const idToDelete = action.id;
+      const indexToDelete = state.saved.indexOf(idToDelete);
+      const savedList = state.saved;
+      if (indexToDelete == -1) return state;
+      return {
+        ...state,
+        saved: [
+          ...savedList.slice(0, indexToDelete),
+          ...savedList.slice(indexToDelete + 1)
+        ]
+      };
+    default:
+      return state;
+  }
+};
+export default listByColumn;
+
+export const getSavedProperties = state => {
+  return state.saved;
+};
+export const getResultsProperties = state => {
   return state.results;
-}
+};
