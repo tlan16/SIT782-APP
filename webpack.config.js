cecb5970517f5ebfacef844914d738cf67b3f804
Frank Lan <franklan118@gmail.com>
2018-08-11 15:34:17 +1000

    fix indentation

--- webpack.config.js
+++ webpack.config.js
@@ -3,49 +3,49 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const path = require('path');
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: './src/index.html',
-    filename: './index.html'
+  template: './src/index.html',
+  filename: './index.html'
 });
 const dotenvPlugin = new Dotenv({
-    safe: true,
+  safe: true,
 });
 
 module.exports = {
-    entry: ['babel-polyfill', './src/index.jsx'],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true,
-                            modules: true,
-                            localIdentName: '[local]___[hash:base64:5]'
-                        }
-                    }]
-
+  entry: ['babel-polyfill', './src/index.jsx'],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: '[local]___[hash:base64:5]'
             }
+          }]
 
-        ]
-    },
-    resolve: {
-      extensions: ['*', '.js', '.jsx'],
-    },
-    plugins: [htmlPlugin, dotenvPlugin]
+      }
+
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  plugins: [htmlPlugin, dotenvPlugin]
 };
