a72ae171b93203630b1e7013485e2d9de931f95e
Frank Lan <franklan118@gmail.com>
2018-08-11 17:23:29 +1000

    add webpack image loader

--- webpack.common.js
+++ webpack.common.js
@@ -37,6 +37,19 @@ const styleLoader = {
 
 }
 
+const imageLoader = {
+  test: /\.(gif|png|jpe?g|svg)$/i,
+  use: [
+    'file-loader',
+    {
+      loader: 'image-webpack-loader',
+      options: {
+        disable: true, // webpack@2.x and newer
+      },
+    },
+  ],
+}
+
 module.exports = {
   entry: ['babel-polyfill', './src/index.jsx'],
   output: {
@@ -47,6 +60,7 @@ module.exports = {
     rules: [
       javascriptLoader,
       styleLoader,
+      imageLoader,
     ],
   },
   resolve: {
