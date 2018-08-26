a2b9377bfc6e38d0593caecbafd0b5ccbed47a35
Frank Lan <franklan118@gmail.com>
2018-08-26 12:57:45 +1000

    don't use subtitle

--- src/screen/profile/index.jsx
+++ src/screen/profile/index.jsx
@@ -6,7 +6,6 @@ import { getAuth } from '../../store/properties/reducer'
 import * as actions from '../../store/properties/actions'
 import TopNav from '../components/topNav'
 import Footer from '../components/footer'
-import Subtitle from '../components/subheader'
 
 class ProfileScreen extends React.Component {
   render() {
@@ -27,10 +26,55 @@ class ProfileScreen extends React.Component {
           <TopNav />
           <div className="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body">
             <div className="m-grid__item m-grid__item--fluid m-wrapper">
-              <Subtitle title="Dashboard" />
               <div className="m-content">
                 <div className="row">
-                  Here is the profile content
+                  <div className="col-md-12">
+                    <div className="m-portlet m-portlet--tab">
+                      <div className="m-portlet__head">
+                        <div className="m-portlet__head-caption">
+                          <div className="m-portlet__head-title">
+                            <span className="m-portlet__head-icon m--hide">
+                              <i className="la la-gear" />
+                            </span>
+                            <h3 className="m-portlet__head-text">
+                              Profile
+                            </h3>
+                          </div>
+                        </div>
+                      </div>
+                      {/* begin::Form */}
+                      <form className="m-form m-form--fit m-form--label-align-right">
+                        <div className="m-portlet__body">
+                          <div className="form-group m-form__group">
+                            <label htmlFor="exampleInputEmail1">Email address</label>
+                            <input type="email" className="form-control m-input m-input--square" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                            <span className="m-form__help">We'll never share your email with anyone else.</span>
+                          </div>
+                          <div className="form-group m-form__group">
+                            <label htmlFor="exampleInputPassword1">Password</label>
+                            <input type="password" className="form-control m-input m-input--square" id="exampleInputPassword1" placeholder="Password" />
+                          </div>
+                          <div className="form-group m-form__group">
+                            <label htmlFor="exampleSelect1">Example select</label>
+                            <select className="form-control m-input m-input--square" id="exampleSelect1">
+                              <option>1</option>
+                              <option>2</option>
+                              <option>3</option>
+                              <option>4</option>
+                              <option>5</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="m-portlet__foot m-portlet__foot--fit">
+                          <div className="m-form__actions">
+                            <button type="reset" className="btn btn-metal">Submit</button>
+                            <button type="reset" className="btn btn-secondary">Cancel</button>
+                          </div>
+                        </div>
+                      </form>
+                      {/* end::Form */}
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
